dfe64c5962d54f4519e597b79fc6a1a2a7bde5bc
Rumiya <littleangel03@mail.ru>
2024-03-31 18:12:59 +0300

    2 домашка с 6-7 пункт

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,29 +1,14 @@
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
 public class TestBase {
     protected WebDriver driver;
-    protected Map<String, Object> vars;
-    protected JavascriptExecutor js;
 
-    @Before
-    public void setUp() {
+    public TestBase() {
+        // Установка WebDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\chromedriver.exe");
         driver = new ChromeDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<>();
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
     }
 
     public void login(Credentials credentials) {
@@ -31,5 +16,19 @@ public class TestBase {
         driver.findElement(By.name("username")).sendKeys(credentials.getUsername());
         driver.findElement(By.name("password")).sendKeys(credentials.getPassword());
         driver.findElement(By.cssSelector(".ui-button-text")).click();
+    }
+
+    public void createEntity(String title, String content) {
+        driver.findElement(By.name("notetitle")).click();
+        driver.findElement(By.name("notetitle")).sendKeys(title);
+        driver.findElement(By.id("textarea")).click();
+        driver.findElement(By.id("textarea")).sendKeys(content);
+        driver.findElement(By.id("autosave")).click();
+        driver.findElement(By.name("makeprivate")).click();
+        driver.findElement(By.xpath("//body[@id='page-wrap']/div[2]/div/div[7]/button/span")).click();
+    }
+
+    public void quit() {
+        driver.quit();
     }
 }
--- src/test/java/TestikTest.java
+++ src/test/java/TestikTest.java
@@ -1,8 +1,29 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 
-
 public class TestikTest extends TestBase {
+
+  @Test
+  public void testEntityCreation() {
+    // Временно повторяем код для авторизации
+    driver.get("https://www.zippyjot.com/");
+    driver.manage().window().setSize(new Dimension(550, 699));
+    Credentials credentials = new Credentials("ruzalina15", "030703");
+    login(credentials);
+
+    // Начало сгенерированного кода из Katalon
+    driver.get("https://www.zippyjot.com/home/");
+    driver.findElement(By.name("notetitle")).click();
+    driver.findElement(By.name("notetitle")).sendKeys("test");
+    driver.findElement(By.id("textarea")).click();
+    driver.findElement(By.id("textarea")).sendKeys("yewgtedwe");
+//    driver.findElement(By.id("autosave")).click();
+    driver.findElement(By.name("makeprivate")).click();
+    driver.findElement(By.xpath("//body[@id='page-wrap']/div[2]/div/div[7]/button/span")).click();
+    // Конец сгенерированного кода из Katalon
+  }
+
 
   @Test
   public void testik() {
@@ -11,4 +32,5 @@ public class TestikTest extends TestBase {
     Credentials credentials = new Credentials("ruzalina15", "030703");
     login(credentials);
   }
+
 }
