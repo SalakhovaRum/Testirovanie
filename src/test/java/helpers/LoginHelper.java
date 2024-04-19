1fafb970035a230b1f3fa12dacd7f9bb3d008006
Rumiya <littleangel03@mail.ru>
2024-04-19 14:03:27 +0300

    4 домашка добавлен тест для удаления

--- src/test/java/helpers/LoginHelper.java
+++ src/test/java/helpers/LoginHelper.java
@@ -14,6 +14,20 @@ public class LoginHelper extends HelperBase {
         driver.get("https://www.zippyjot.com/");
     }
 
+    public void enterUsername(String username) {
+        driver.findElement(By.name("username")).sendKeys(username);
+    }
+
+    // Метод для ввода пароля
+    public void enterPassword(String password) {
+        driver.findElement(By.name("password")).sendKeys(password);
+    }
+
+    // Метод для нажатия кнопки входа
+    public void clickLoginButton() {
+        driver.findElement(By.cssSelector(".ui-button-text")).click();
+    }
+
     public void login(String username, String password) {
         driver.findElement(By.linkText("Log In")).click();
         driver.findElement(By.name("username")).sendKeys(username);
--- src/test/java/helpers/NavigationHelper.java
+++ src/test/java/helpers/NavigationHelper.java
@@ -2,7 +2,10 @@ package helpers;
 
 import appmanager.ApplicationManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class NavigationHelper extends HelperBase {
 
@@ -14,6 +17,45 @@ public class NavigationHelper extends HelperBase {
         driver.get("https://www.zippyjot.com/");
     }
 
+    public void hoverOverElement(String elementText) {
+        WebElement element = driver.findElement(By.linkText(elementText));
+        Actions builder = new Actions(driver);
+        builder.moveToElement(element).perform();
+    }
+
+    public void clickElement(String elementText) {
+        WebElement element = driver.findElement(By.linkText(elementText));
+        element.click();
+    }
+
+    public void scrollToElement(String elementText) {
+        WebElement element = driver.findElement(By.linkText(elementText));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void scrollToTop() {
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0);");
+    }
+
+    public void sendKeysToElementById(String elementId, String keys) {
+        WebElement element = driver.findElement(By.id(elementId));
+        element.sendKeys(keys);
+    }
+
+    public void scrollToElementById(String elementId, int xOffset, int yOffset) {
+        WebElement element = driver.findElement(By.id(elementId));
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(" + element.getLocation().x + ", " + element.getLocation().y + ")");
+    }
+
+    public void clickElementByName(String elementName) {
+        WebElement element = driver.findElement(By.name(elementName));
+        element.click();
+    }
+
+    public void sendKeysToElementByName(String elementName, String keys) {
+        WebElement element = driver.findElement(By.name(elementName));
+        element.sendKeys(keys);
+    }
 
     public void test(String title, String content) {
         driver.findElement(By.name("notetitle")).click();
