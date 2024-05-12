4a58ac502f5608dd63cd9f94f18d03b213da7906
Rumiya <littleangel03@mail.ru>
2024-05-12 14:08:57 +0300

    6 домашка | 2 пункт

--- src/test/java/helpers/LoginHelperTest.java
+++ src/test/java/helpers/LoginHelperTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-
 public class LoginHelperTest {
 
     private ApplicationManager applicationManager;
@@ -38,5 +37,36 @@ public class LoginHelperTest {
         driver.findElement(By.linkText("Sign Out")).click();
     }
 
+    @Test
+    public void testLoginWithValidData() {
+        loginHelper.logout(); // Принудительный выход перед тестом
+        String validUsername = "ruzalina15";
+        String validPassword = "030703";
+        loginHelper.login(validUsername, validPassword);
 
+        // Получаем экземпляр WebDriver из LoginHelper
+        WebDriver driver = loginHelper.getDriver();
+
+        // Утверждение: Проверяем, что после входа появляется кнопка "Sign Out"
+        boolean isSignedIn = driver.findElement(By.linkText("Sign Out")).isDisplayed();
+        Assert.assertTrue("Пользователь не вошел в систему!", isSignedIn);
+
+        // Выполняем необходимые действия после входа
+        driver.findElement(By.linkText("Sign Out")).click();
+    }
+
+    @Test
+    public void testLoginWithInvalidData() {
+        loginHelper.logout(); // Принудительный выход перед тестом
+        String invalidUsername = "ruzalina";
+        String invalidPassword = "15";
+        loginHelper.login(invalidUsername, invalidPassword);
+
+        // Получаем экземпляр WebDriver из LoginHelper
+        WebDriver driver = loginHelper.getDriver();
+
+        // Утверждение: Проверяем, что после входа не появляется кнопка "Sign Out"
+        boolean isSignedIn = driver.findElements(By.linkText("Sign Out")).isEmpty();
+        Assert.assertFalse("Пользователь вошел в систему с неверными данными!", isSignedIn);
+    }
 }
